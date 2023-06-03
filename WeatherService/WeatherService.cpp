97f4c0c3fa6ea1f0c5e98323fad419d69da01afc
Max Rubtsov <maxjt808@gmail.com>
2023-06-03 19:35:14 +0300

    stay on created xml and json objects both

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
-
+#include "Weather.h"
+#include "JsonService.h"
+#include "XmlService.h"
 int main()
 {
-    
+    JsonService js;
+    Weather w = js.getWeather("weather.json");
+
+    XmlService xs;
+    Weather w = xs.getWeather("weather.xml");
+
 }
